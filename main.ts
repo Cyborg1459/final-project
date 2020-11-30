cff8fcd19ab698b2773eb3f64cd0df991be860e8
Cyborg1459 <72390558+Cyborg1459@users.noreply.github.com>
2020-11-30 08:24:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -24,23 +24,39 @@ let mySprite = sprites.create(img`
     ........................
     ........................
     `, SpriteKind.Player)
-let mySprite3 = sprites.create(img`
-    . . . . . . 2 2 2 2 . . . . . . 
-    . . . . 2 2 3 3 3 3 2 e . . . . 
-    . . . 2 3 d 1 1 d d 3 2 e . . . 
-    . . 2 3 1 d 3 3 3 d d 3 e . . . 
-    . 2 3 1 3 3 3 3 3 d 1 3 b e . . 
-    . 2 1 d 3 3 3 3 d 3 3 1 3 b b . 
-    2 3 1 d 3 3 1 1 3 3 3 1 3 4 b b 
-    2 d 3 3 d 1 3 1 3 3 3 1 3 4 4 b 
-    2 d 3 3 3 1 3 1 3 3 3 1 b 4 4 e 
-    2 d 3 3 3 1 1 3 3 3 3 1 b 4 4 e 
-    e d 3 3 3 3 d 3 3 3 d d b 4 4 e 
-    e d d 3 3 3 d 3 3 3 1 3 b 4 b e 
-    e 3 d 3 3 1 d d 3 d 1 b b e e . 
-    . e 3 1 1 d d 1 1 1 b b e e e . 
-    . . e 3 3 3 3 3 3 b e e e e . . 
-    . . . e e e e e e e e e e . . . 
+let burger = sprites.create(img`
+    ...........ccccc66666...........
+    ........ccc4444444444666........
+    ......cc444444444bb4444466......
+    .....cb4444bb4444b5b444444b.....
+    ....eb4444b5b44444b44444444b....
+    ...ebb44444b4444444444b444446...
+    ..eb6bb444444444bb444b5b444446..
+    ..e6bb5b44444444b5b444b44bb44e..
+    .e66b4b4444444444b4444444b5b44e.
+    .e6bb444444444444444444444bb44e.
+    eb66b44444bb444444444444444444be
+    eb66bb444b5b44444444bb44444444be
+    fb666b444bb444444444b5b4444444bf
+    fcb666b44444444444444bb444444bcf
+    .fbb6666b44444444444444444444bf.
+    .efbb66666bb4444444444444444bfe.
+    .86fcbb66666bbb44444444444bcc688
+    8772effcbbbbbbbbbbbbbbbbcfc22778
+    87722222cccccccccccccccc22226678
+    f866622222222222222222222276686f
+    fef866677766667777776667777fffef
+    fbff877768f86777777666776fffffbf
+    fbeffeefffeff7766688effeeeefeb6f
+    f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+    f66ddfffffeeeffeffeeeeeffeedb46e
+    .c66ddd4effffffeeeeeffff4ddb46e.
+    .fc6b4dddddddddddddddddddb444ee.
+    ..ff6bb444444444444444444444ee..
+    ....ffbbbb4444444444444444ee....
+    ......ffebbbbbb44444444eee......
+    .........fffffffcccccee.........
+    ................................
     `, SpriteKind.Player)
 let mySprite2 = sprites.create(img`
     . . . . . . . . . . . . . . . . 
