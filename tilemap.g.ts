b3d5fa583a00cf1d578d8c97132b7b8d14bfb141
DoggoRegime777 <75024705+DoggoRegime777@users.noreply.github.com>
2021-01-04 08:00:42 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -96,6 +96,8 @@ namespace myTiles {
     export const tile46 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile47 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile48 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
