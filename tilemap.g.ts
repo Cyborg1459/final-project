163cb7ad9d772e9878f8f8905f28e1c128c64cd3
Kewk9 <74912854+Kewk9@users.noreply.github.com>
2021-01-18 18:33:50 -0500

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -250,6 +250,12 @@ namespace myTiles {
     export const tile123 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile124 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile125 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile126 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile127 = image.ofBuffer(hex``);
 
     helpers.registerTilemapFactory(function(name: string) {
         switch(helpers.stringTrim(name)) {
